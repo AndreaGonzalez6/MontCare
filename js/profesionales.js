79a525e9d773b45f3341f6d4e47b2350f4032fb8
AndreaGonzalez6 <paulagonzalez282002@gmail.com>
2023-09-19 10:34:24 -0500

    cambios

--- js/profesionales.js
+++ js/profesionales.js
@@ -14,3 +14,18 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
   
+  var swiper1 = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
