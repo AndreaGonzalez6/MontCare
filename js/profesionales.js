d69877627bc3c26b4fa7e25204a52349dca3e050
AndreaGonzalez6 <paulagonzalez282002@gmail.com>
2023-10-09 16:50:48 -0500

    mejora

--- js/profesionales.js
+++ js/profesionales.js
@@ -1,15 +1,10 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+// Inicializa el carousel de Bootstrap
+var carouselRadiologo = new bootstrap.Carousel(document.getElementById('carouselRadiologo'), {
+  interval: 5000, // Cambia el intervalo de tiempo entre las diapositivas según sea necesario
+  wrap: true // Si deseas que el carrusel vuelva al principio después de llegar al final
+});
+
+var carouselCardiologo = new bootstrap.Carousel(document.getElementById('carouselCardiologo'), {
+  interval: 5000, // Cambia el intervalo de tiempo entre las diapositivas según sea necesario
+  wrap: true // Si deseas que el carrusel vuelva al principio después de llegar al final
 });
