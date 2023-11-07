d8f62c736afcea43d697265cf1278ed235233459
AndreaGonzalez6 <paulagonzalez282002@gmail.com>
2023-11-07 11:01:04 -0500

    citas

--- reservar.php
+++ reservar.php
@@ -5,6 +5,7 @@ include("citas-medicas/includes/db.php");
 
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
 <script>
+  var a;
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -15,7 +16,7 @@ include("citas-medicas/includes/db.php");
       allDaySlot: false,
       events: 'reservas/cargar_reservas.php',
       dateClick: function(info) {
-        var a = info.dateStr;
+        a = info.dateStr;
         const fechaComoCadena = a;
         var numeroDia = new Date(fechaComoCadena).getDay();
         var dias = ['LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES'];
@@ -79,20 +80,20 @@ include('reservas/parte.php');
             <center><b>Turno Mañana</b></center>
             <br>
             <div class="d-grid gap-2">
-              <button class="btn btn-success" id="btn_h1" type="button">08:00 - 08:20</button>
-              <button class="btn btn-success" id="btn_h2" type="button">08:20 - 08:40</button>
-              <button class="btn btn-success" id="btn_h1" type="button">08:40 - 09:00</button>
-              <button class="btn btn-success" id="btn_h2" type="button">09:00 - 09:20</button>
+              <button class="btn btn-success" id="btn_h1" data-bs-dismiss="modal" type="button">08:00 - 08:20</button>
+              <button class="btn btn-success" id="btn_h2" data-bs-dismiss="modal" type="button">08:20 - 08:40</button>
+              <button class="btn btn-success" id="btn_h3" data-bs-dismiss="modal" type="button">08:40 - 09:00</button>
+              <button class="btn btn-success" id="btn_h4" data-bs-dismiss="modal" type="button">09:00 - 09:20</button>
             </div>
           </div>
           <div class="col-md-6">
           <center><b>Turno Tarde</b></center>
             <br>
           <div class="d-grid gap-2">
-              <button class="btn btn-success" id="btn_h1" type="button">08:00 - 08:20</button>
-              <button class="btn btn-success" id="btn_h2" type="button">08:20 - 08:40</button>
-              <button class="btn btn-success" id="btn_h1" type="button">08:40 - 09:00</button>
-              <button class="btn btn-success" id="btn_h2" type="button">09:00 - 09:20</button>
+              <button class="btn btn-success" id="btn_h5" data-bs-dismiss="modal" type="button">08:00 - 08:20</button>
+              <button class="btn btn-success" id="btn_h6" data-bs-dismiss="modal" type="button">08:20 - 08:40</button>
+              <button class="btn btn-success" id="btn_h7" data-bs-dismiss="modal" type="button">08:40 - 09:00</button>
+              <button class="btn btn-success" id="btn_h8" data-bs-dismiss="modal" type="button">09:00 - 09:20</button>
             </div>
           </div>
         </div>
@@ -104,3 +105,66 @@ include('reservas/parte.php');
     </div>
   </div>
 </div>
+
+
+
+
+
+
+<div class="modal fade" id="modal_formulario" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalLabel">Reserva tu cita para el día <span id="dia_de_la_semana"></span></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <form action="">
+            <div class="row">
+              <div class="col-md-6">
+                <label for="">Nombre</label>
+                <input type="text" class="form-control">
+              </div>
+              <div class="col-md-6">
+                <label for="">Tipo de servicio</label>
+                <select name="" id="" class="form-control">
+                  <option value="PEDIATRIA">PEDIATRÍA</option>
+                  <option value="TRAUMATOLOGIA">TRAUMATOLOGÍA</option>
+                  <option value="CARDIOLOGIA">CARDIOLOGÍA</option>
+                </select>
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-md-6">
+                <label for="">Fecha de la cita</label>
+                <input type="text" class="form-control" id="fecha_cita" disabled>
+              </div>
+              <div class="col-md-6">
+                <label for="">Hora de la cita</label>
+                <input type="text" class="form-control" id="hora_cita" disabled>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Guardar cambios</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- segundo modal según el horario -->
+<script>
+  $('#btn_h1').click(function(){
+    $('#modal_formulario').modal("show");
+    $('#fecha_cita').val(a);
+    var h1 = "08:00 - 08:20";
+    $('#hora_cita').val(h1);
+  });
+</script>
+
+
